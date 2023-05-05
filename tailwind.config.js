ccdab33d7f527c75da053a6eaeb93c110890fac8
abduhalil <abduhalilismoilov560@gmail.com>
2023-05-05 06:17:44 +0500

    feature/tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    container: {
+      padding: "2rem",
+    },
+  },
+  plugins: [],
+};
